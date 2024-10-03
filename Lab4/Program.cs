16f8f6a91845ba5ae2bee042aaf60c8952e9b4d1
dangthaisontd <dangthaisontd@gmail.com>
2024-10-03 20:41:24 +0700

    add conpleted

---
+++ Lab4/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int Scale = 23000;
+        Console.WriteLine("Nhap vao so tien do ma ban co");
+        int usd = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("So tien viet ma ban co la{0}",Scale*usd);
+    }
+}

b700446bc6943972111817bd518c378210529d4d
dangthaisontd <dangthaisontd@gmail.com>
2024-10-03 21:11:21 +0700

    add

---
+++ Lab5/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap diem trung binh cua sinh vien");
+        float score = float.Parse(Console.ReadLine());
+        if( 0<=score&&score<5)   Console.WriteLine("Yeu");
+        else if( 5<=score&&score<7) Console.WriteLine("TB");
+        else if(7<=score&&score<8)  Console.WriteLine("Kha");
+        else if (8<=score && score<=10) Console.WriteLine("Gioi");
+        else Console.WriteLine("Nhap sai gia tri diem so");
+    }
+}
